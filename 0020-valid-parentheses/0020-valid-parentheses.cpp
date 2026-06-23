bf2aba6a0b05e71f8e3e6a97511789174b1663f3
Mohit vanzara <160529916+mohitvanzara@users.noreply.github.com>
2026-06-23 19:18:03 +0530

    Time: 0 ms (100%), Space: 9 MB (18.85%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.size();
+        stack<char> st;
+        for(int i =0 ;i<n ;i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                //opening bracket mate
+                st.push(s[i]);
+            }
+            else{
+                //closing mate
+                if(st.empty()) return false;
+                else if(st.top()=='(' && s[i]==')' || 
+                        st.top()=='{' && s[i]=='}' ||
+                        st.top()=='[' && s[i]==']')
+                        {
+                            st.pop();
+
+                        }
+                else {
+                    return false;
+                }
+            }
+        }
+        return st.size()==0;
+    }
+};
