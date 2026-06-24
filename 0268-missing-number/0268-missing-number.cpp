f6a2b75275ec76b9bed0f34fc2eb4016fc5ca878
Mohit vanzara <160529916+mohitvanzara@users.noreply.github.com>
2026-06-24 19:00:55 +0530

    Time: 0 ms (100%), Space: 21.9 MB (12.55%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -6,9 +6,9 @@ public:
         int xor2 = 0;
         for(int i = 0 ;i<nums.size() ;i++){
             xor2 = xor2 ^ nums[i];
-            xor1 = xor1 ^ i;
+            xor1 = xor1 ^ (i+1);
         }
-        xor1 = xor1^ nums.size();
+        // xor1 = xor1^ nums.size();
         return xor1^xor2;
         
         
